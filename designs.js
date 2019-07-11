2811284d89cc985cda73601e13c380e1891f5217
Ahad Alanazi <42279786+ahadalmu@users.noreply.github.com>
2019-07-11 10:12:42 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,20 +1,27 @@
+// define a new variables in javascript that get element by ID in HTML
 const submit = document.getElementById("submit");
 const pixelCanvas = document.getElementById("pixelCanvas");
-// Select size input
 // When size is submitted by the user, call makeGrid()
 function makeGrid(event) {
-    // Your code goes here!
+    // stop the default action of an element from happening
     event.preventDefault();
+    // to clear the canvas
     pixelCanvas.innerHTML = "";
+    // Select size input
     const height = document.getElementById("inputHeight").value;
     const width = document.getElementById("inputWidth").value;
+    //  the loop that make the canvas
     for (let i = 0; i < height; i++) {
+        // to create row at the canvas
         const tRow = document.createElement("tr");
         for (let j = 0; j < width; j++) {
+            // to create cell inside the row
             const cell = document.createElement("td");
+            // Add an event listener that color the cell when a user clicks the button
             cell.addEventListener('click', function (event2) {
                 // Select color input
                 const color = document.getElementById("colorPicker").value;
+                // to chane the cell back ground color
                 event2.target.style.backgroundColor = color;
             });
             tRow.appendChild(cell);
@@ -22,4 +29,5 @@ function makeGrid(event) {
         pixelCanvas.appendChild(tRow);
     }
 }
+// Add an event listener that make a canvas when a user clicks the button
 submit.addEventListener('click', makeGrid);
